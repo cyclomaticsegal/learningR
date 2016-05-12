3570b95ffd070cf7e2dee852dc87214bed7a5f0d
Simon Segal <simon@qcat.com.au>
2016-05-13 09:31:32 +1000

    functions exercises

---
+++ learningFunctions.R
@@ -0,0 +1,72 @@
+#functions
+
+log(27)
+
+exp(3)
+
+root225 <- sqrt(225)
+sq15 <- 15^2
+
+root225
+sq15
+
+abs(-9)
+
+answer <- 2+2
+sin(17 + answer)
+
+#function to create objects
+x <- 10
+D <- 1/sqrt(x)
+
+#arguments
+
+round(14.5378, digits = 2)
+
+round(14.5378, digits = 1)
+
+round(14.5378) #default rounding no digits
+
+#signif for significant numbers
+X1 <- 3583.5192
+
+round(X1, digits = 3)
+signif(X1, digits = 3)
+#will round the whole number 3583 portion up
+signif(X1, 2)
+
+#exercises
+
+log10(X1)
+
+sqrt(x2)
+
+x2^0.5
+
+sum(x1, x2)
+
+X4 <- 67.8953
+
+round(X4)
+
+round(X4, 3)
+
+floor(X4)
+
+ceiling(X4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
