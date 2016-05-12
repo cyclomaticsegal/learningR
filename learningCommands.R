8486be0b0ac30a53c202b2352b2eff87b0377568
Simon Segal <simon@qcat.com.au>
2016-05-12 15:57:19 +1000

    cor for lizards and general command line learning

---
+++ learningCommands.R
@@ -0,0 +1,31 @@
+#learning commands
+
+6 + 3
+
+3 ^ 3
+
+22 * 15 + 6
+
+22 * (15 +16)
+
+
+22 ^ 2 + 7
+
+22 ^ (2+7) #produces scientific notation
+
+#exercises 1:
+
+3 * 5
+
+5 ^ 2
+
+(8+22) * 3
+
+(12-2.8) ^1.5 + 1.3924
+
+#keep in memory
+
+answer <- 2+2
+
+
+
