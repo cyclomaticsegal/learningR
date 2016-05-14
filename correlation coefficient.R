766e875d57a08bdf0a29ef3407242ee94bbf2c18
Simon Segal <simon@qcat.com.au>
2016-05-14 21:47:37 +1000

    matrices

--- correlation coefficient.R
+++ correlation coefficient.R
@@ -1,5 +1,6 @@
 # nearest neighbour correlation coefficient test on lizards testosterone and nearest.neighbour fx.
 nearest.neighbour <- c(4,7,3,5,8,3,7,4,3,5,5,4)
 testosterone <- c(22.2, 26.1, 21.5, 23.8, 23.8, 20.5, 24.6, 22.6, 19.9, 23.9, 19.8, 21.3)
+plot(nearest.neighbour, testosterone)
 cor.test(nearest.neighbour, testosterone)
 
---
+++ matrices.R
@@ -0,0 +1,68 @@
+mat1 <- matrix(data = seq(1,12), nrow=3, ncol=4, dimnames = list(c("Row 1", "Row 2", "Row 3"), c("Col 1", "Col 2", "Col 3", "Col 4")), byrow=TRUE)
+
+#use byrow to make the matrix order by row otherwise default is by column
+
+#using arithmatic on matrix
+
+mat2 <- mat1 / 3
+
+#use args to functions
+
+sqrt(mat1)
+
+mat1 + mat2
+
+V1 <- seq(1, 11, by=2)
+
+mat1 <- matrix(V1, nrow=2)
+
+mat2 <- matrix(V1, nrow=2, byrow=TRUE)
+
+#what kind of numeric is the vector using
+
+X1 <- 1:12
+mode(X1)
+class(X1)
+
+X1 <- c("Red", "Red", "Blue", "Blue", "Green")
+
+class(X1)
+
+#factors - things divided into 'classes' are called categorical or nominal data and stored as 'factors'
+#nominal data can be input as numbers and then made a factor with the factor function
+#easier to pass a vector to the factor function
+
+X1 <- factor(X1)
+X1
+
+#check its a factor
+
+is.factor(X1)
+X2 <- 2
+is.factor(X2)
+
+#R mode() of a factor will be numeric even if coded as chars because R recodes variables and assings number to each level
+
+as.numeric(X1) #produces 3 3 1 1 2 as the characters are recoded in alphabetic order
+
+#Data Frame is like a matrix with different data types in different columns
+
+Number <- c(1,2,3,4)
+Diet <- c("Poor", "Poor", "Good", "Good")
+Sex <- c("M", "F", "M", "F")
+Weight <- c(156, 180, 167, 190)
+Fat.content <- c(34, 43, 40, 43)
+Morph <- c("Winged", "Winged", "Wingless", "Intermediate")
+crickets <- data.frame(Number, Diet, Sex, Weight, Fat.content, Morph)
+
+#note below the $ symbol to point into the frame properties
+is.factor(crickets$Diet)
+
+
+
+
+
+
+
+
+
